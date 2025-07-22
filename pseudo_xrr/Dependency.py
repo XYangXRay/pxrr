42792676e0699a8b8e60c105790534a4286214e6
AlexP226 <alexpalps11@gmail.com>
2025-07-22 15:47:43 -0400

    added fall back case for metadata file parameter

--- pseudo_xrr/Dependency.py
+++ pseudo_xrr/Dependency.py
@@ -1,6 +1,6 @@
 from pseudo_xrr.data_io import load_data, load_metadata, binning_GIXOS_data, remove_negative_2theta, dependency_real_space_2theta, GIXOS_data_plot_prep, GIXOS_RF_and_SF, rect_slit_function, conversion_to_reflectivity, GIXOS_file_output, Qxy_GIXOS_data_plot_prep, create_dependency_models
 from pseudo_xrr.plots import GIXOS_vs_Qz_plot, GIXOS_vs_Qxy_plot, dependency_plot
-def dependency_wrapper(metadata_file):
+def dependency_wrapper(metadata_file = './testing_data/gixos_metadata.yaml'):
     importGIXOSdata, importbkg = load_data(metadata_file)
     metadata = load_metadata(metadata_file)
     importGIXOSdata, importbkg = binning_GIXOS_data(importGIXOSdata, importbkg)
@@ -8,7 +8,7 @@ def dependency_wrapper(metadata_file):
     metadata = dependency_real_space_2theta(metadata)
     GIXOS, DSbetaHW = Qxy_GIXOS_data_plot_prep(importGIXOSdata, importbkg, metadata, tt_step)
     GIXOS_vs_Qz_plot(GIXOS, metadata)
-    GIXOS_vs_Qxy_plot(GIXOS, metadata) # will need to add vars to metadata for this function to work
+    GIXOS_vs_Qxy_plot(GIXOS, metadata)
     model, assume_model, CWM_model = create_dependency_models(GIXOS, metadata, DSbetaHW)
     dependency_plot(GIXOS, metadata, model, assume_model, CWM_model)
     return
--- pseudo_xrr/slit.py
+++ pseudo_xrr/slit.py
@@ -1,8 +1,8 @@
 from pseudo_xrr.data_io import load_data, load_metadata, binning_GIXOS_data, remove_negative_2theta, real_space_2theta, GIXOS_data_plot_prep, GIXOS_RF_and_SF, rect_slit_function, conversion_to_reflectivity, GIXOS_file_output
 from pseudo_xrr.plots import GIXOS_data_plot, R_data_plot, R_pseudo_data_plot
-def rect_slit_wrapper():
-    importGIXOSdata, importbkg = load_data('./testing_data/gixos_metadata.yaml')
-    metadata = load_metadata('./testing_data/gixos_metadata.yaml')
+def rect_slit_wrapper(metadata_file='./testing_data/gixos_metadata.yaml'):
+    importGIXOSdata, importbkg = load_data(metadata_file)
+    metadata = load_metadata(metadata_file)
     importGIXOSdata, importbkg = binning_GIXOS_data(importGIXOSdata, importbkg)
     importGIXOSdata, importbkg, tt_step = remove_negative_2theta(importGIXOSdata, importbkg)
     metadata = real_space_2theta(metadata)
